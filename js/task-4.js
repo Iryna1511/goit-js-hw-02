46d79be6c64e7062404aa94dde23f1510f48ff1b
Iryna1511 <irina.shkolnikova93@gmail.com>
2024-01-17 12:42:35 +0200

    task 1 done

--- js/task-4.js
+++ js/task-4.js
@@ -1,30 +1,23 @@
-'use strict';
-
-
+"use strict";
 
 function getShippingCost(country) {
-    let price;
-    switch (country) {
-        case 'China':
-            price = 100;
-            return `Shipping to ${country} will cost ${price} credits`;
-            break;
-        case 'Chile':
-            price = 250;
-            return `Shipping to ${country} will cost ${price} credits`;
-            break;
-        case 'Australia':
-            price = 170;
-            return `Shipping to ${country} will cost ${price} credits`;
-            break;
-        case 'Jamaica':
-            price = 120;
-            return `Shipping to ${country} will cost ${price} credits`;
-            break;
-        default:
-            return "Sorry, there is no delivery to your country";  
-    
-    }
+  let price;
+  switch (country) {
+    case "China":
+      price = 100;
+      return `Shipping to ${country} will cost ${price} credits`;
+    case "Chile":
+      price = 250;
+      return `Shipping to ${country} will cost ${price} credits`;
+    case "Australia":
+      price = 170;
+      return `Shipping to ${country} will cost ${price} credits`;
+    case "Jamaica":
+      price = 120;
+      return `Shipping to ${country} will cost ${price} credits`;
+    default:
+      return "Sorry, there is no delivery to your country";
+  }
 }
 
 console.log(getShippingCost("Australia"));
@@ -33,5 +26,3 @@ console.log(getShippingCost("China"));
 console.log(getShippingCost("Chile"));
 console.log(getShippingCost("Jamaica"));
 console.log(getShippingCost("Sweden"));
-
-
